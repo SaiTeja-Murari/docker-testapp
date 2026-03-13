5645bde3371067f2ca3bfccb1e70db01a5b2f94f
SaiTeja-Murari <murari.saiteja01@gmail.com>
2026-03-13 10:29:11 +0530

    small change in mongodb login

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-const MONGO_URL = "mongodb://admin:saiteja@localhost:27017";
+const MONGO_URL = "mongodb://root:mini_project@localhost:27017";
 const client = new MongoClient(MONGO_URL);
 
 let db;
